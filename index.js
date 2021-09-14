34743b5a306e6c673178fbaf264bbe65c3a3bc70
0rmindo <86755009+0rmindo@users.noreply.github.com>
2021-09-14 13:27:24 -0300

    Nova implementação
    Implementação de acordo com a sujestão do supervisor

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
 const express = require('express');
+
 const app = express();
+
 const port = 3333;
 
-app.get('/soma', (request, response) => {
-  return response.status(400).send('status code 400 (Bad Request)');
-});
+const sumRoute = require('./routes/sum-route.js');
 
-app.get('/soma/:param', (request, response) => {
-  return response.status(400).send('status code 400 (Bad Request)');
-});
-
-app.get('/soma/:num1/:num2', (request, response) => {
-  const num1 = Number(request.params.num1);
-  const num2 = Number(request.params.num2);
-  const result = num1 + num2;
-
-  const isZero = result === 0;
-  const isTruthyNumber = Boolean(result);
-
-  if (isTruthyNumber || isZero) {
-    return response.send(`${result}`);
-  }
-  
-  return response.status(400).send('status code 400 (Bad Request)');
-});
+app.use('/soma', sumRoute);
 
 app.listen(port, () => {
+
   console.log(`Backend is running at localhost: ${port}`);
+
 });
