6843a4dc311cc7178951a039f330860fc7b6d224
0rmindo <86755009+0rmindo@users.noreply.github.com>
2021-11-30 16:09:53 -0300

    Update sum-route.test.js

--- routs/sum-route.test.js
+++ routs/sum-route.test.js
@@ -3,5 +3,5 @@ const { router } = require('./sum-route.js');
 test('Cinco mais dois resultado sete', () => {
   const params = { num1: 5, num2: 2 };
   
-  expect(route(params)).toBe(7);
+  expect(router(params)).toBe(7);
 });
