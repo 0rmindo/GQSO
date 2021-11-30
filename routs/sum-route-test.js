f5b058d268527151a47543dea00fe956fb0d42ab
0rmindo <86755009+0rmindo@users.noreply.github.com>
2021-11-30 13:56:03 -0300

    Update sum-route-test.js

--- routs/sum-route-test.js
+++ routs/sum-route-test.js
@@ -1,19 +1,7 @@
-const { sum, resultIsOk } = require('./sum-route.js');
+const { route } = require('./sum-route.js');
 
 test('Cinco mais dois resultado sete', () => {
   const params = { num1: 5, num2: 2 };
   
-  expect(sum(params)).toBe(7);
-});
-
-test('Sum result is ok', () => {
-  expect(resultIsOk(0)).toBe(true);
-  expect(resultIsOk(5)).toBe(true);
-  expect(resultIsOk(-5)).toBe(true);
-});
-
-test('Sum result is not ok', () => {
-  expect(resultIsOk('')).toBe(false);
-  expect(resultIsOk('a')).toBe(false);
-  expect(resultIsOk(NaN)).toBe(false);
+  expect(route(params)).toBe(7);
 });
